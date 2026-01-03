96fcaabb994b08540deee9f97bea0f073dd3f448
Abdullah A. <alhariri.abdu.2018@gmail.com>
2026-01-03 13:09:19 +0100

    Removing azan lock on demand

--- index.js
+++ index.js
@@ -66,9 +66,15 @@ function azan(fajr=false) {
         return;
     }
     azanPlaying = true;
+    setInterval(() => {
+        azanPlaying = false;
+        console.log('Azan ended, lock removed')
+    }, 5 * 60 * 1000); // 5 minutes lock
+    console.log('Azan playing, lock created')
 
     const file = fajr ? 'fajr' : 'regular';
     const volume = (fajr ? '4' : '1') * volumeScaler;
+    spawn('pactl', ['set-sink-volume', '@DEFAULT_SINK@', '100%'])
     spawn('ffplay', ['-nodisp', '-volume', volume, '-autoexit', __dirname + `/azan/${file}.mp3`]);
 }
 
